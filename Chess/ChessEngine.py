c8a1da86c5b591e20a7986238771c4bd2b27fc12
Caitlin Hornbeek <125606362+Chornbeek@users.noreply.github.com>
2024-07-01 11:22:37 -0400

    Initial commit

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,19 @@
+# This class is responsible for storing all the information about the current state of a chess game. It will also be responsible
+#  for determining the valid moves at the current state
+
+class GameState():
+    def __init__(self):
+        # Board is 8x8  2d list, each element of the list has 2 characters.
+        # The first character represents the color of the piece, 'b' or 'w'
+        # The second character represents the type of the piece, 'K', 'Q', 'R', 'N', 'p'
+        # "--" - represents an empty spcae with no piece
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
---
+++ Chess/ChessMain.py
@@ -0,0 +1,23 @@
+# This is our main driver file. It will be responsible for handling user input and displaying current GameState object.
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8 # Dimensions 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # Animations later on
+IMAGES = {}
+
+
+# Initialize a global dictionary of images. This will be called exactly once in the main
+def load_images():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+# This will be main driver for our code. This will handle user input and updating the graphics
+def main():
+    p.init()
+
+
